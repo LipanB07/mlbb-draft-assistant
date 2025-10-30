12e0c6e1bc467a52fbc0d44a1d8ff72a254099a4
vincenzo <vincenzofarro01@gmail.com>
2025-10-31 00:51:10 +0100

    c16 - design change: added my personal picture to the ko-fi cta

--- app/page.jsx
+++ app/page.jsx
@@ -9,10 +9,18 @@ export default function Home() {
       <Hero />
       <a
         href="https://ko-fi.com/vincenzofarro"
-        target="_blank" rel="noopener noreferrer"
-        className="btn"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn ko-fi-btn"
       >
         <span className="truncate">Support Me on Ko-fi</span>
+        <Image
+          src="/images/vincenzoImage.png"
+          alt="Vincenzo Farro"
+          width={44}
+          height={44}
+          className="profile-pic"
+        />
       </a>
     </main>
   );
